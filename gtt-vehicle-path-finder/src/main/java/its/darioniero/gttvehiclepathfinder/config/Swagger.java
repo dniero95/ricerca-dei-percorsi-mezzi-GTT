6effd13dccb3d59a70efbe2157c4cff823ca4f3f
Dario Niero <dario.niero@enginius.com>
2023-07-05 00:49:10 +0200

    update swagger config

--- gtt-vehicle-path-finder/src/main/java/its/darioniero/gttvehiclepathfinder/config/Swagger.java
+++ gtt-vehicle-path-finder/src/main/java/its/darioniero/gttvehiclepathfinder/config/Swagger.java
@@ -1,4 +1,17 @@
 package its.darioniero.gttvehiclepathfinder.config;
 
-public class SwaggerConfig {
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class Swagger {
+    @Bean
+    public OpenAPI baseOpenAPI(){
+        return new OpenAPI().info(new Info().title("GTT vehicle path finder APIs")
+                .version("1.0.0").description("RESTful APIs for the GTT vehicle path finder website"));
+    }
 }
