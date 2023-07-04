2146644fe8bb82604ba0ba565f6752fa1d7a9680
Dario Niero <dario.niero@enginius.com>
2023-07-04 22:36:31 +0200

    import spring project

---
+++ gtt-vehicle-path-finder/src/main/java/its/darioniero/gttvehiclepathfinder/GttVehiclePathFinderApplication.java
@@ -0,0 +1,13 @@
+package its.darioniero.gttvehiclepathfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GttVehiclePathFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GttVehiclePathFinderApplication.class, args);
+	}
+
+}
---
+++ gtt-vehicle-path-finder/src/test/java/its/darioniero/gttvehiclepathfinder/GttVehiclePathFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package its.darioniero.gttvehiclepathfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GttVehiclePathFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
