7cf453fe8453e7255db1164a08dd98a04233e163
Dario Niero <dario.niero@enginius.com>
2023-07-07 23:56:22 +0200

    WIP: render all the fermate in html

--- gtt-vehicle-path-finder/src/main/java/its/darioniero/gttvehiclepathfinder/controllers/integration/ViewController.java
+++ gtt-vehicle-path-finder/src/main/java/its/darioniero/gttvehiclepathfinder/controllers/integration/ViewController.java
@@ -1,16 +1,28 @@
 package its.darioniero.gttvehiclepathfinder.controllers.integration;
 
+import its.darioniero.gttvehiclepathfinder.entities.Fermata;
+import its.darioniero.gttvehiclepathfinder.services.FermataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 public class ViewController {
 
+    @Autowired
+    FermataService fermataService;
+
     @GetMapping
-    String getLinee(){
+    public String getLinee(Model model) {
+        List<Fermata> fermate = fermataService.fetchAllFermata();
+        model.addAttribute("fermate", fermate);
         return "fermate";
     }
+
 
     @GetMapping("/linee")
     String getFermate() {
